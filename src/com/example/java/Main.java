6040d885c7dd98ffdf83c67de47ce1e19ad351f8
kkruse13 <kkruse13@students.mchenry.edu>
2017-09-17 15:59:05 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] strings = {"ice cream","cones","freezer","scooper","cash register"};
+	for (String materials : strings) {
+        System.out.println(materials);
+    }
+    }
+}
